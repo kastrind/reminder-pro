2c30c057e7f68b65e239ea7a95b028f92493f862
Dimitris <kastrind@gmail.com>
2017-11-24 16:57:09 +0200

    Initial commit, adding times to reminders.

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+
+export const addReminder = (text, dueDate) => {
+  const action = {
+    type: ADD_REMINDER,
+    text: text,
+    dueDate: dueDate
+  }
+  console.log('action in addReminder', action);
+  return action;
+}
+
+export const deleteReminder = (id) => {
+  const action = {
+    type: DELETE_REMINDER,
+    id: id
+  }
+  return action;
+}
